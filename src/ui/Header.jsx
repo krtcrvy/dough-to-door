0077de5cf6ccdd632ad51c40c8beec05a7efa860
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-10-04 23:01:26 +0800

    refactor: Update header text to "Dough to Door"

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header className="flex items-center justify-between border-stone-200 border-b bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
-        Fast React Pizza Co.
+        Dough to Door
       </Link>
 
       <SearchOrder />
