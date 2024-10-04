31bf466e898073a84e533c79857bbd7189625dd6
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-10-04 23:06:19 +0800

    refactor: Remove unused "@million/lint" dependency

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import MillionLint from "@million/lint";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [MillionLint.vite(), react()],
+  plugins: [react()],
 });
